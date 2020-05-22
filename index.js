878d176a14d448316afb872f7568abfedf4d840f
masja1623 <62559800+masja1623@users.noreply.github.com>
2020-05-22 12:50:22 +0300

    JS step 1

---
+++ index.js
@@ -0,0 +1,52 @@
+/* Task 2
+alert("Pashkevych");*/
+
+/* Task 3
+let data = "Maryna";
+let text = "New text";
+alert(data);
+alert(text);
+data = text;
+alert(data);*/
+
+/* Task 4
+let user = {
+    string: "Maryna",
+    Number: 25,
+    Boolean: true,
+    undefined: undefined,
+    null: null
+};*/
+
+/* Task 5
+let isAdult = confirm("Are you an adult?");
+console.log(isAdult); */
+
+/* Task 6
+let FirstName = "Maryna";
+let LastName = "Pashkevych";
+let group = "510 JavaScript";
+let BirthYear = 1997;
+let married = true;
+console.log (typeof BirthYear, typeof married, typeof FirstName, typeof LastName, typeof group);
+let user;
+let age = null;
+console.log (typeof user, typeof age);*/
+
+/*Task 7
+let login = prompt("Enter your Login", "User");
+let email = prompt("Your Email", "usermale@gmail.com");
+let password = prompt("Password", "qwerty");
+alert(`Dear ${login} your email is ${email}, your password is ${password}.`)*/
+
+/*Task 8
+let seconds = 60;
+let hourMinutes = 60;
+let dayHours = 24;
+let dayMonth = 30;
+console.log("Кількість секунд в годині:" + seconds*hourMinutes);
+console.log("Кількість секунд в добі:" + seconds*hourMinutes*dayHours);
+console.log("Кількість секунд в місяці:" + seconds*hourMinutes*dayHours*dayMonth);*/
+
+
+
